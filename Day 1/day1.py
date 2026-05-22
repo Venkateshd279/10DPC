daedf27535787f6647a5f0c857f6b155c6f288ba
Venkatesh Dhanapalraj <venkateshdhanapalraj@Venkateshs-MacBook-Air.local>
2026-05-22 20:32:06 +0530

    day 1 added

---
+++ Day 1/day1.py
@@ -0,0 +1,20 @@
+print ("Hello world")
+
+# variables 
+
+a = 10
+b = 20
+c = a
+
+print (a,b, c)
+
+# Operators 
+
+a  = 20 
+b  = 10 
+
+print ("addition = ", a + b)
+print ("subtraction = ", a - b)
+print ("multiplication = ", a * b)
+print ("division = ", a // b)
+print ("modulus = ", a % b)
